968d423677e9a2968db34750ec05ab39a00e7060
krupali <krupalisuhagiya2@gmail.com>
2025-01-08 10:56:23 +0530

    third  commit

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://martial-art-hub-backend.onrender.com/api"
-// const baseUrl = "http://localhost:4000/api"
+// const baseUrl = "https://martial-art-hub-backend.onrender.com/api"
+
+const baseUrl = "http://localhost:4000/api"
 
 export default baseUrl
